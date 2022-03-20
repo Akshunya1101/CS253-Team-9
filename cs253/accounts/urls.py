a03748b29125731d3f84e35f2792b4b4ca206c61
Samar-23 <samarpreet20@iitk.ac.in>
2022-03-21 00:08:39 +0530

    Update delete added

--- cs253/accounts/urls.py
+++ cs253/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import (Sell_Create,Sell_List,Sell_Details)
+from .views import (Sell_Create,Sell_List,Sell_Details,Sell_Delete, Sell_Update)
 from django.contrib.auth.decorators import login_required
 
 app_name = 'accounts'
@@ -11,7 +11,15 @@ urlpatterns = [
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
 	path('buy/',login_required(Sell_List.as_view()), name="buy"),
+	path('search/',views.search, name="search"),
 	path('sell/',login_required(Sell_Create.as_view()), name="sell"),
 	path('details/<int:pk>',login_required(Sell_Details.as_view()), name="item_details"),
 	path('rent/', views.rent, name="rent"), 
+	path('enterRoom/', views.enterRoom, name="enterRoom"), 
+	path('<str:room>/', views.room, name='room'),
+    path('enterRoom/checkview', views.checkview, name='checkview'),
+    path('send', views.send, name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+	path('delete/<int:pk>', Sell_Delete.as_view(), name='delete_item'),
+	path('update/<int:pk>', Sell_Update.as_view(), name='update_item'),
 ]
